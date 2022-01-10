fcd085d9a6d88a941b1d7ba1c13d47f9b636d35b
winsightnsk <winsight@mail.ru>
2022-01-10 19:24:15 +0700

    Итоговая: Перед финишом

--- lib/loadjson.dart
+++ lib/loadjson.dart
@@ -3,6 +3,8 @@ import 'package:berkana/widget_totalbar.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
+import 'class_users.dart';
+
 const String urlGetUsersList = "https://jsonplaceholder.typicode.com/users";
 
 class LoadJSON extends StatefulWidget {
@@ -65,26 +67,6 @@ class _ListJSONState extends State<ListJSON> {
   }
 }
 
-class User {
-  final int id;
-  final String name;
-  final String email;
-
-  User({
-    required this.id,
-    required this.name,
-    required this.email,
-  });
-
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(
-      id: json['id'],
-      name: json['name'],
-      email: json['email'],
-    );
-  }
-}
-
 ListView _usersListView(BuildContext context, data) {
   return ListView.builder(
       itemCount: data.length,
@@ -97,13 +79,47 @@ ListTile _userListTile(BuildContext context, String title, String subtitle, Icon
   return ListTile(
     title: Text(
       title,
-      style: Theme.of(context).textTheme.bodyText2?.copyWith(fontSize: 33, color: Colors.brown),
+      style: Theme.of(context).textTheme.bodyText2?.copyWith(
+          fontSize: 33,
+          color: Colors.brown,
+          shadows: <Shadow>[
+            const Shadow(
+              offset: Offset(3.0, 3.0),
+              blurRadius: 10.0,
+              color: Colors.brown,
+            ),
+          ],
+      ),
     ),
-    subtitle: Text(subtitle, style: Theme.of(context).textTheme.bodyText1?.copyWith(color: Colors.teal)),
-    leading: Icon(
-      icon,
-      size: 40,
-      color: Colors.teal[500],
+    subtitle: Text(
+        subtitle,
+        style: Theme.of(context).textTheme.bodyText1?.copyWith(
+          color: Colors.teal,
+          shadows: <Shadow>[
+            const Shadow(
+              offset: Offset(3.0, 3.0),
+              blurRadius: 10.0,
+              color: Colors.teal,
+            ),
+          ],
+        ),
+    ),
+    leading:
+    Container(
+        decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromRGBO(0, 31, 232, 0.2),
+                blurRadius: 5.0,
+              ),
+            ]
+        ),
+        child: Icon(
+          icon,
+          size: 40,
+          color: Colors.teal[500],
+        ),
     ),
     onTap: ()=>showDetales(context),
   );
